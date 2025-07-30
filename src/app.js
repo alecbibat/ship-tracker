a93cd598c2edb637bfd7261993fe0f1a9c19571c
alecbibat <alecbibat@gmail.com>
2025-07-30 10:10:50 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -111,3 +111,9 @@ app.get('/', (req, res) => {
 });
 
 module.exports = app;
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
